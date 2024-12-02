52724492a945af977c52d980c77ddf7014289337
niusz <163yourba@163.com>
2024-12-02 17:37:21 +0800

    feat: 添加urls,修改admin.py

--- Blog/mysite/blog/models.py
+++ Blog/mysite/blog/models.py
@@ -1,13 +1,25 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts',
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -17,6 +29,8 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT,
     )
+    objects = models.Manager()
+    published = PublishedManager()
     class Meta:
         ordering = ['-publish']
         indexes = [
