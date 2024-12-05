5966eccb5c892da5f17634e4b2ec6d52282f186c
niusz <163yourba@163.com>
2024-12-05 17:39:34 +0800

    feat: 评论框完成

--- Blog/mysite/blog/admin.py
+++ Blog/mysite/blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Comment, Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']
@@ -13,3 +13,8 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
     show_facets = admin.ShowFacets.ALWAYS
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
--- Blog/mysite/blog/forms.py
+++ Blog/mysite/blog/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+
+from .models import Comment
+
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
@@ -7,3 +10,8 @@ class EmailPostForm(forms.Form):
         required=False,
         widget=forms.Textarea,
     )
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
